50cd66ea5b577a3d86b0361d0de3b162b774dc0a
Sungsu Kim <kim.sungsu6732@gmail.com>
2021-10-19 17:24:58 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+// application/x-www-form-urlencoded 형식의 데이터 가져오기
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json 형식의 데이터 가져오기
+app.use(bodyParser.json());
+
 mongoose
-  .connect(
-    "mongodb+srv://SungsuKim:kim.sungsu6732@boilerplate.p6x5z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB Connected.."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("안녕");
+});
+
+app.post("/register", (req, res) => {
+  // 회원 가입 시 필요한 정보들을 클라이언트에서 가져오면 그것들을 DB에 넣어줍니다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
